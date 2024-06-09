70b4ce11c8711390af928e48b3f41b72a5253034
FredThx <fredthx@gmail.com>
2024-06-09 20:28:11 +0200

    optimize

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -11,12 +11,32 @@ Une simulation du jeu de la vie
 # ajouter compteur de tours, temps/tour
 # multithread (un regle 1 , l'autre regle 2)
 # Afficher en couleur différentes les naissances et morts (et rafraichir avant maj)
+# determiner quand terminé
+# temps mini pour un tour
 
 import time
 import os
 import random
 from tkinter import *
 
+exec_time_wrappers = []
+
+def exec_time(func):
+    def wrapper(*args, **kwargs):
+        t_start = time.time()
+        r = func(*args, **kwargs)
+        t_end = time.time()
+        wrapper.total_duration += t_end - t_start
+        #print("Execution of " + func.__name__ + " took cumuled " + str(round(wrapper.total_duration)) + " seconds")
+        return r
+    wrapper.total_duration = 0
+    wrapper.exec_time_name = func.__name__
+    exec_time_wrappers.append(wrapper)
+    return wrapper
+
+def show_exec_time(nb_tours):
+    for wrapper in exec_time_wrappers:
+        print(f"Execution of {wrapper.exec_time_name} took {wrapper.total_duration/nb_tours:.3f} seconds per turn.")
 
 class Plateau(object):
     '''Le plateau de jeu
@@ -31,6 +51,7 @@ class Plateau(object):
         self.aire = Canvas(self.root, height = self.hauteur * taille_y, width = self.largeur * taille_x)
         self.aire.grid()
         Button(self.root, text = "Go!", command = self.run).grid()
+        self.offsets = (P(-1,-1),P(0,-1),P(1,-1),P(-1,0),P(1,0),P(-1,1),P(0,1),P(1,1))
 
 
 
@@ -63,8 +84,11 @@ class Plateau(object):
         #input("Press Enter to continue...")
         while True:
             self.un_tour()
-            self.root.update()
+            self.update_ui()
 
+    @exec_time
+    def update_ui(self):
+        self.root.update()
 
     def un_tour(self):
         ''' génère un tour complet
@@ -76,12 +100,15 @@ class Plateau(object):
         self.regle1()
         self.naissances()
         self.clean()
+        show_exec_time(self.tour)
+        pass
 
     def print(self):
         txt = str(self)
         os.system('cls')
         print(txt)
 
+    @exec_time
     def regle1(self):
         '''Applique la règle n° 1:
             Si cellule morte (ou pas de cellule dans notre cas) a exactement 3 voisines
@@ -91,6 +118,7 @@ class Plateau(object):
             if self.nb_voisines(position_vide)==3:
                 self.cellules[position_vide]= Cellule(self.aire, position_vide)
 
+    @exec_time
     def regle2(self):
         '''Applique la règle 2 :
             Une cellule vivante le reste uniqument si elle a 2 ou 3 voisines exactement
@@ -121,7 +149,7 @@ class Plateau(object):
                 position = P(x,y)
                 if position not in self.cellules:
                     yield position
-
+    @exec_time
     def positions_vides(self):
         '''Renvoie un dict des positions vides du plateau.
         Solution un peu mieux que la version 0
@@ -131,22 +159,23 @@ class Plateau(object):
             positions_vides.update({pos : True for pos in self.voisines(position) if pos not in self.cellules or self.cellules[pos].vivante != True})
         return positions_vides
 
-
+    @exec_time
     def nb_voisines(self, position):
         '''Renvoie le nombre de voisines limité à 4 (4 equivaut à (4 ou 5 ou 6 ou 7 ou 8))
         '''
         nb = 0
         for voisine in self.voisines(position):
-            if voisine in self.cellules and self.cellules[voisine].vivante is not None:
+            cellule = self.cellules.get(voisine)
+            if cellule and cellule.vivante is not None:
                 nb +=1
-                if nb > 3:
-                    break
+                if nb == 4:
+                    return 4
         return nb
 
     def voisines(self, position):
         '''générateur de voisines
         '''
-        for offset in [P(-1,-1),P(0,-1),P(1,-1),P(-1,0),P(1,0),P(-1,1),P(0,1),P(1,1)]:
+        for offset in self.offsets:
             yield self.espace_infini(position + offset)
 
     def espace_infini(self, position):
@@ -166,10 +195,10 @@ class Plateau(object):
         '''remplit aleatoirement le Plateau
             taux : entre 0 (peu de cellules) et 1 (un max de cellules)
         '''
-        for x in range(int(self.largeur / 2)):
-            for y in range(int(self.hauteur / 2)):
+        for x in range(self.largeur):
+            for y in range(self.hauteur):
                 if random.random()< taux:
-                    self.add(P(int(self.largeur / 4) + x,int(self.hauteur / 4) + y))
+                    self.add(P(x,y))
 
 
 class Cellule(object):
@@ -222,11 +251,11 @@ class P(tuple):
         return self[1]
 
 if __name__ == "__main__":
-    plateau = Plateau(300,180)
+    plateau = Plateau(300,100)
     #n=24
     #for i in range(n):
         #plateau.add(P(int(plateau.largeur/2 - n/2 )+i,int(plateau.hauteur / 2)))
     #Un planeur
     plateau.add([P(3,11),P(1,12),P(3,12),P(2,13),P(3,13)])
-    plateau.aleat_polulate(0.2)
+    plateau.aleat_polulate(0.1)
     plateau.root.mainloop()
